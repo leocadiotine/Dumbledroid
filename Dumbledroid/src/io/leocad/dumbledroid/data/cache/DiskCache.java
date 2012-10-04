05538d1708ca6efe4a1f8396cb25a50b2f78e596
Leocadio Tiné <leocadiotine@gmail.com>
2012-10-04 14:53:07 -0300

    Closes the file streams after loading/saving files.

--- Dumbledroid/src/io/leocad/dumbledroid/data/cache/DiskCache.java
+++ Dumbledroid/src/io/leocad/dumbledroid/data/cache/DiskCache.java
@@ -14,55 +14,64 @@ import android.util.Log;
 public class DiskCache {
 
 	private static final String TAG = "DiskCache";
-	
+
 	private static DiskCache INSTANCE;
 	/**
 	 * @param ctx ALWAYS USE getApplicationContext() here.
 	 * @return
 	 */
 	public static DiskCache getInstance(Context ctx) {
-		
+
 		if (INSTANCE == null) {
 			INSTANCE = new DiskCache(ctx);
 		}
-		
+
 		return INSTANCE;
 	}
-	
-	private FileController mFileCtrl;
-	
+
+	private final FileController mFileCtrl;
+
 	private DiskCache(Context ctx) {
 		mFileCtrl = new FileController(ctx);
 	}
-	
+
 	public void cache(String key, AbstractModel model) {
-		
+
 		String fileName = String.valueOf(key.hashCode());
-		
+
 		ModelHolder holder = new ModelHolder(model, System.currentTimeMillis());
-		
+
 		try {
 			FileOutputStream fos = mFileCtrl.getFileOutputStream(fileName);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(holder);
-			
+
+			oos.flush();
+			oos.close();
+
+			fos.flush();
+			fos.close();
+
 		} catch (IOException e) {
 			Log.w(TAG, "The file " + fileName + "couldn't be cached. Does this app have permission to ACCESS_EXTERNAL_STORAGE?", e);
 		}
 	}
-	
+
 	public ModelHolder getCached(String key) {
-		
+
 		String fileName = String.valueOf(key.hashCode());
-		
+
 		try {
 			FileInputStream fis = mFileCtrl.getFileInputStream(fileName);
 			ObjectInputStream ois = new ObjectInputStream(fis);
-			
+
 			ModelHolder holder = (ModelHolder) ois.readObject();
-			
+
+			ois.close();
+			fis.close();
+
 			return holder;
-		
+
 		} catch (Exception e) {
 			Log.w(TAG, "Can't access file: " + fileName, e);
 			return null;
