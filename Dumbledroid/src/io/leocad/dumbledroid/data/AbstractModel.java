ef59466775dc59b238de1f836b5468d005e6ad12
Leocadio Tiné <leocadiotine@gmail.com>
2013-02-12 18:32:05 -0500

    Adds the missing imports from pull request #7.

--- Dumbledroid/src/io/leocad/dumbledroid/data/AbstractModel.java
+++ Dumbledroid/src/io/leocad/dumbledroid/data/AbstractModel.java
@@ -1,6 +1,7 @@
 package io.leocad.dumbledroid.data;
 
 import io.leocad.dumbledroid.net.HttpMethod;
+import io.leocad.dumbledroid.net.NoConnectionException;
 
 import java.io.Serializable;
 import java.util.List;
@@ -9,26 +10,28 @@ import org.apache.http.NameValuePair;
 import org.apache.http.protocol.HTTP;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 
 public abstract class AbstractModel implements Serializable {
-	
+
 	private static final long serialVersionUID = -889584042617358518L;
 
 	private static final String DEFAULT_ENCODING = HTTP.UTF_8;
-	
+
 	public String url;
 	public String encoding;
 	public String error;
 
 	public long cacheDuration;
-	
+
 	protected abstract DataType getDataType();
-	
+
 	protected AbstractModel(String url) {
 		this(url, DEFAULT_ENCODING);
 	}
-	
+
 	protected AbstractModel(String _url, long _cacheDuration) {
 		this(_url, DEFAULT_ENCODING, _cacheDuration);
 	}
@@ -36,32 +39,33 @@ public abstract class AbstractModel implements Serializable {
 	protected AbstractModel(String _url, String _encoding) {
 		this(_url, _encoding, -1L);
 	}
-	
+
 	protected AbstractModel(String _url, String _encoding, long _cacheDuration) {
 		url = _url;
 		encoding = _encoding;
 		cacheDuration = _cacheDuration;
 	}
-	
+
 	public void load(Context ctx) throws Exception {
 		load(ctx, null);
 	}
-	
+
 	protected void load(Context ctx, List<NameValuePair> params) throws Exception {
 		load(ctx, params, null);
 	}
-	
+
 	protected void load(Context ctx, List<NameValuePair> params, HttpMethod method) throws Exception {
 		checkConnection(ctx);
 		DataController.load(ctx, this, getDataType(), params, method);
 	}
 
 	private void checkConnection(Context ctx) throws NoConnectionException {
-		ConnectivityManager cm = (ConnectivityManager)
-		ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
-	        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-	        if (netInfo == null || !netInfo.isConnectedOrConnecting()) {
-	            throw new NoConnectionException();
-	        }
+
+		ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		
+		if (netInfo == null || !netInfo.isConnectedOrConnecting()) {
+			throw new NoConnectionException();
+		}
 	}
 }
