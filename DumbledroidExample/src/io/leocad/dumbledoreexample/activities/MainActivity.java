ad20ca8b46cf9187d28c1bdf903c6d57183ed015
Leocadio Tiné <leocadiotine@gmail.com>
2013-02-27 19:05:20 -0500

    Adds the main activity.

---
+++ DumbledroidExample/src/io/leocad/dumbledoreexample/activities/MainActivity.java
@@ -0,0 +1,27 @@
+package io.leocad.dumbledoreexample.activities;
+
+import io.leocad.dumbledoreexample.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+	public void onJediClicked(View v) {
+		
+	}
+	
+	public void onSithClicked(View v) {
+		
+	}
+	
+	public void onTwitterClicked(View v) {
+		
+	}
+}
