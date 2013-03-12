55e3db90c96750994e84c6b1f4fcf5b8cae9e5a1
Leocadio Tiné <leocadiotine@gmail.com>
2013-03-12 14:16:10 -0400

    Extracts core code to DumbledroidClassCreator.

--- EclipsePlugin/src/io/leocad/dumbledroidplugin/wizards/NewModelWizard.java
+++ EclipsePlugin/src/io/leocad/dumbledroidplugin/wizards/NewModelWizard.java
@@ -1,15 +1,10 @@
 package io.leocad.dumbledroidplugin.wizards;
 
-import io.leocad.dumbledroidplugin.exceptions.InvalidUrlException;
-import io.leocad.dumbledroidplugin.exceptions.UnsupportedContentTypeException;
+import io.leocad.dumbledroidplugin.core.DumbledroidClassCreator;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -58,7 +53,7 @@ public class NewModelWizard extends Wizard implements INewWizard {
 			public void run(IProgressMonitor monitor) throws InvocationTargetException {
 				
 				try {
-					doFinish(url, isPojo, monitor);
+					DumbledroidClassCreator.create(url, isPojo, monitor);
 				} catch (Exception e) {
 					throw new InvocationTargetException(e);
 				} finally {
@@ -79,91 +74,6 @@ public class NewModelWizard extends Wizard implements INewWizard {
 		}
 		
 		return true;
-	}
-
-	private void doFinish(String urlAddress, boolean isPojo, IProgressMonitor monitor) throws CoreException, UnsupportedContentTypeException, InvalidUrlException {
-		
-		try {
-		monitor.beginTask("Validating URL…", 4);
-		
-		URL url = null;
-		try {
-			url = new URL(urlAddress);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			//Will never happen. The URL was already validated on UrlInputPage
-		}
-		
-		HttpURLConnection connection = null;
-		try {
-			connection = (HttpURLConnection)  url.openConnection();
-			connection.connect();
-		} catch (IOException e) {
-			//e.printStackTrace();
-			throw new InvalidUrlException();
-		}
-		
-		boolean isJson = isJson(connection);
-		monitor.worked(1);
-		
-		monitor.setTaskName("Fetching URL contents…");
-		
-		if (isJson) {
-			
-		} else {
-			
-		}
-		
-		
-		monitor.worked(1);
-		monitor.setTaskName("Writing to file…");
-		Thread.sleep(2000);
-//		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-//		IResource resource = root.findMember(new Path(containerName));
-//		if (!resource.exists() || !(resource instanceof IContainer)) {
-//			throwCoreException("Container \"" + containerName
-//					+ "\" does not exist.");
-//		}
-//		IContainer container = (IContainer) resource;
-//		final IFile file = container.getFile(new Path(fileName));
-//		try {
-//			InputStream stream = openContentStream();
-//			if (file.exists()) {
-//				file.setContents(stream, true, true, monitor);
-//			} else {
-//				file.create(stream, true, monitor);
-//			}
-//			stream.close();
-//		} catch (IOException e) {
-//		}
-		
-//		getShell().getDisplay().asyncExec(new Runnable() {
-//			public void run() {
-//				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-//				try {
-//					IDE.openEditor(page, file, true);
-//				} catch (PartInitException e) {
-//				}
-//			}
-//		});
-		monitor.worked(1);
-		} catch (InterruptedException e) {
-			
-			e.printStackTrace();
-		}
-	}
-
-	private boolean isJson(HttpURLConnection connection) throws UnsupportedContentTypeException {
-		
-		String contentType = connection.getContentType();
-		
-		if (contentType.contains("json")) {
-			return true;
-		} else if (contentType.contains("xml")) {
-			return false;
-		}
-		
-		throw new UnsupportedContentTypeException(contentType);
 	}
 
 	/**
